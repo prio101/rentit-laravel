f30720d22288c76eea6dce2859148d7d2d294eec
prio101 <miprio101@gmail.com>
2015-12-14 23:12:31 +0600

    Deleted Commented Bookings Seed data

--- database/seeds/BookingsTableSeeder.php
+++ database/seeds/BookingsTableSeeder.php
@@ -17,21 +17,10 @@ class BookingsTableSeeder extends Seeder
         // Bookings Table
 
 
-//
-//        DB::table('bookings')->insert([
-//            'client_id'     => random_int(1 , 1000),
-//            'car_id'        => random_int(1 , 1000) ,
-//            'receive_place' => Faker\Provider\bn_BD\Address::state() ,
-//            'leaving_place' => Faker\Provider\bn_BD\Address::state() ,
-//            'receive_date'  => \Faker\Provider\DateTime::date('Y-m-d') ,
-//            'leaving_date'  => \Faker\Provider\DateTime::dateTimeThisMonth() ,
-//            'price_plan'    => random_int(1 , 10),
-//            'promotion_code'=> random_int(10 , 20),
-//            'created_at'    => \Faker\Provider\DateTime::date('Y-m-d'),
-//            'updated_at'    => \Faker\Provider\DateTime::date('Y-m-d'),
-//            'status'        => random_int(1 , 3)
-//        ]);
 
+/*
+ * Creating 100 seed data for the bookings table
+ * */
         for($i = 0  ; $i <= 100 ; $i++){
 
         DB::table('bookings')->insert([
