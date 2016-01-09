5e8c4047b6e8204a8ce753b1bcef3c2d15353d55
prio101 <miprio101@gmail.com>
2016-01-10 03:19:42 +0600

    Added and working the auth register and login module

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,54 +12,58 @@
                     <input type="email" name="email" class="form-control" placeholder="email@email.com" autofocus>
                     <br>
                     <input type="password" name="password" class="form-control" placeholder="Password">
-                    {{--<label class="checkbox">--}}
-		                {{--<span class="pull-right">--}}
-		                    {{--<a data-toggle="modal" href="login.html#myModal"> Forgot Password?</a>--}}
-
-		                {{--</span>--}}
-                    {{--</label>--}}
 
 
+                    <br>
                     <button class="btn btn-theme btn-block"  type="submit"><i class="fa fa-lock"></i> SIGN IN</button>
+
                     <hr>
 
-                    {{--<div class="login-social-link centered">--}}
-                        {{--<p>or you can sign in via your social network</p>--}}
-                        {{--<button class="btn btn-facebook" type="submit"><i class="fa fa-facebook"></i> Facebook</button>--}}
-                        {{--<button class="btn btn-twitter" type="submit"><i class="fa fa-twitter"></i> Twitter</button>--}}
-                    {{--</div>--}}
-                    {{--<div class="registration">--}}
-                        {{--Don't have an account yet?<br/>--}}
-                        {{--<a class="" href="{{url('auth/register')}}">--}}
-                            {{--Create an account--}}
-                        {{--</a>--}}
-                    {{--</div>--}}
+                    <div class="login-social-link centered">
+                        <p>or you can sign in via your social network</p>
+                        <button class="btn btn-facebook" type="submit"><i class="fa fa-facebook"></i> Facebook</button>
+                        <button class="btn btn-twitter" type="submit"><i class="fa fa-twitter"></i> Twitter</button>
+                    </div>
+                    <div class="registration">
+                        Don't have an account yet?<br/>
+                        <a class="btn btn-facebook btn-block " href="{{url('auth/register')}}">
+                            Create an account
+                        </a>
+                    </div>
+                    <label class="checkbox">
+		                <span class="">
+		                    <a data-toggle="modal" class="btn btn-info btn-clear-g btn-block" href="#myModal"> Forgot Password?</a>
 
+		                </span>
+                    </label>
                 </div>
 
-                {{--<!-- Modal -->--}}
-                {{--<div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="myModal" class="modal fade">--}}
-                    {{--<div class="modal-dialog">--}}
-                        {{--<div class="modal-content">--}}
-                            {{--<div class="modal-header">--}}
-                                {{--<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>--}}
-                                {{--<h4 class="modal-title">Forgot Password ?</h4>--}}
-                            {{--</div>--}}
-                            {{--<div class="modal-body">--}}
-                                {{--<p>Enter your e-mail address below to reset your password.</p>--}}
-                                {{--<input type="text" name="email" placeholder="Email" autocomplete="off" class="form-control placeholder-no-fix">--}}
 
-                            {{--</div>--}}
-                            {{--<div class="modal-footer">--}}
-                                {{--<button data-dismiss="modal" class="btn btn-default" type="button">Cancel</button>--}}
-                                {{--<button class="btn btn-theme" type="button">Submit</button>--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-                {{--<!-- modal -->--}}
 
             </form>
+
+
+            <!-- Modal -->
+            <div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="myModal" class="modal fade">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                            <h4 class="modal-title">Forgot Password ?</h4>
+                        </div>
+                        <div class="modal-body">
+                            <p>Enter your e-mail address below to reset your password.</p>
+                            <input type="text" name="email" placeholder="Email" autocomplete="off" class="form-control placeholder-no-fix">
+
+                        </div>
+                        <div class="modal-footer">
+                            <button data-dismiss="modal" class="btn btn-default" type="button">Cancel</button>
+                            <button class="btn btn-theme" type="button">Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- modal -->
 
         </div>
     </div>
@@ -67,5 +71,5 @@
 
     <!-- resources/views/auth/login.blade.php -->
 
-    
+
 @stop
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,29 +1,45 @@
-<!-- resources/views/auth/register.blade.php -->
+@extends('login-layout')
 
-<form method="POST" action="/auth/register">
-    {!! csrf_field() !!}
+@section('auth-section')
+    <div id="login-page">
+        <div class="container">
 
-    <div>
-        Name
-        <input type="text" name="name" value="{{ old('name') }}">
+            <form class="form-login" method="POST" action="/auth/register ">
+                {!! csrf_field() !!}
+
+                <h2 class="form-login-heading">sign up now</h2>
+                <div class="login-wrap">
+                    <input type="text" name="name" class="form-control" placeholder="Name" autofocus>
+                    <br>
+                    <input type="email" name="email" class="form-control" placeholder="email@email.com" >
+                    <br>
+                    <input type="password" name="password" class="form-control" placeholder="Password">
+                    <br>
+                    <input type="password" name="password_confirmation" class="form-control" placeholder="Confirm Password" >
+                    <br>
+
+                    <br>
+                    <button class="btn btn-theme btn-block"  type="submit"><i class="fa fa-lock"></i> SIGN UP</button>
+
+                    <hr>
+
+
+
+
+                </div>
+
+
+
+            </form>
+
+
+
+
+        </div>
     </div>
 
-    <div>
-        Email
-        <input type="email" name="email" value="{{ old('email') }}">
-    </div>
 
-    <div>
-        Password
-        <input type="password" name="password">
-    </div>
+    <!-- resources/views/auth/login.blade.php -->
 
-    <div>
-        Confirm Password
-        <input type="password" name="password_confirmation">
-    </div>
 
-    <div>
-        <button type="submit">Register</button>
-    </div>
-</form>
+@stop
